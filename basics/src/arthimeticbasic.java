4ca77a34ae07978b85974b92a189755c9ba7b892
vamshi <nvamshidharreddy7262@gmail.com>
2026-02-04 09:06:56 +0530

    Initial commit

---
+++ basics/src/arthimeticbasic.java
@@ -0,0 +1,8 @@
+public class arthimeticbasic {
+    public static void main(String[] args) {
+        System.out.println(2+5);
+        System.out.println(2*5);
+        System.out.println(2/5);
+        System.out.println(2-5);
+    }
+}
